0edbae87b3c66f777084f949aa2fe93365593364
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2023-02-12 07:01:05 +0300

    FT-26 api module refactored

--- fintrack-api/fintrack-rest/src/main/java/com/maemresen/fintrack/rest/FintrackApplication.java
+++ fintrack-api/fintrack-rest/src/main/java/com/maemresen/fintrack/rest/FintrackApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = "com.maemresen.bt")
-@EnableJpaRepositories(basePackages = "com.maemresen.bt")
-@SpringBootApplication(scanBasePackages = "com.maemresen.bt")
-public class BudgetTrackingApplication {
+@EntityScan(basePackages = "com.maemresen.fintrack")
+@EnableJpaRepositories(basePackages = "com.maemresen.fintrack")
+@SpringBootApplication(scanBasePackages = "com.maemresen.fintrack")
+public class FintrackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BudgetTrackingApplication.class, args);
+        SpringApplication.run(FintrackApplication.class, args);
     }
 }
 
