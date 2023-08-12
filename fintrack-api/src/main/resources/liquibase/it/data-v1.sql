349c4e4c7627fa66032eef6238b7982b67f1e2c2
Emre Sen <maemresen@yazilim.vip>
2023-08-12 12:02:28 +0300

    IT fixed

--- fintrack-api/src/main/resources/liquibase/it/data-v1.sql
+++ fintrack-api/src/main/resources/liquibase/it/data-v1.sql
@@ -4,15 +4,15 @@ VALUES (1, 'Test Budget 1');
 
 -- Statement August 2023 - 1
 INSERT INTO public.statement (id, created_date, updated_date, description, amount, currency, type, date, category, budget_id)
-VALUES (3, '2023-08-06 00:21:46.000000', '2023-08-06 00:21:48.000000', 'Test Expense 2', 100, 'EUR', 'EXPENSE', '2023-08-06 00:22:11.000000', null, 1);
+VALUES (1, '2023-08-06 00:21:46.000000', '2023-08-06 00:21:48.000000', 'Test Expense 2', 100, 'EUR', 'EXPENSE', '2023-08-06 00:22:11.000000', null, 1);
 
 -- Statement August 2023 - 2
 INSERT INTO public.statement (id, created_date, updated_date, description, amount, currency, type, date, category, budget_id)
-VALUES (4, '2023-08-06 00:21:46.000000', '2023-08-06 00:21:48.000000', 'Test Income 2', 100, 'EUR', 'EXPENSE', '2023-08-06 00:22:11.000000', null, 1);
+VALUES (2, '2023-08-06 00:21:46.000000', '2023-08-06 00:21:48.000000', 'Test Income 2', 100, 'EUR', 'EXPENSE', '2023-08-06 00:22:11.000000', null, 1);
 
 -- Statement September 2023 - 1
 INSERT INTO public.statement (id, created_date, updated_date, description, amount, currency, type, date, category, budget_id)
-VALUES (5, '2023-08-06 00:21:46.000000', '2023-08-06 00:21:48.000000', 'Test Expense 3', 100, 'EUR', 'EXPENSE', '2023-09-06 00:22:11.000000', null, 1);
+VALUES (3, '2023-08-06 00:21:46.000000', '2023-08-06 00:21:48.000000', 'Test Expense 3', 100, 'EUR', 'EXPENSE', '2023-09-06 00:22:11.000000', null, 1);
 
 -- Reset sequences
 SELECT setval('budget_id_seq', (SELECT MAX(id) FROM budget));
