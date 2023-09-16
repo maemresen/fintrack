fcc19d7d73dc5af1214704627a5d2ba4177380dd
Emre Sen <maemresen@yazilim.vip>
2023-09-16 15:22:52 +0300

    reset project

--- fintrack-api/src/main/java/com/maemresen/fintrack/api/service/ErrorCodeService.java
+++
@@ -1,9 +0,0 @@
-package com.maemresen.fintrack.api.service;
-
-import com.maemresen.fintrack.api.dto.ErrorCodeDto;
-
-import java.util.List;
-
-public interface ErrorCodeService {
-    List<ErrorCodeDto> findAll();
-}
