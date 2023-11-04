d163928741fc5bd2678d6f116ee4fc07823e45cb
Emre Sen <maemresen@yazilim.vip>
2023-11-05 01:49:45 +0200

    maven test profile updated

---
+++ fintrack-api/src/test/java/com/maemresen/fintrack/api/service/HelloTest.java
@@ -0,0 +1,13 @@
+package com.maemresen.fintrack.api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HelloTest {
+
+    @Test
+    void test1(){
+        assertTrue(true);
+    }
+}
