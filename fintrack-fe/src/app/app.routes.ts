d036ab0772d648ec79aad3f7331dd000a76d9177
Emre Sen <maemresen@yazilim.vip>
2023-11-26 20:58:03 +0200

    component folder created and existing components refactored

--- fintrack-fe/src/app/app.routes.ts
+++ fintrack-fe/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import {Routes} from '@angular/router';
-import {NotFoundComponent} from "@app/page/not-found/not-found.component";
-import {HomeComponent} from "@app/page/home/home.component";
+import {NotFoundComponent} from "@app/component/page/not-found/not-found.component";
+import {HomeComponent} from "@app/component/page/home/home.component";
 
 export const routes: Routes = [
   {path: "", component: HomeComponent},
