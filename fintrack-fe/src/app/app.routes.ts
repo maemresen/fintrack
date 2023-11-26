a004468926a0f805a540963a2fd381813acb3e56
Emre Sen <maemresen@yazilim.vip>
2023-11-26 20:21:16 +0200

    routes and example page components created

--- fintrack-fe/src/app/app.routes.ts
+++ fintrack-fe/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {NotFoundComponent} from "@app/page/not-found/not-found.component";
+import {HomeComponent} from "@app/page/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "emre", component: HomeComponent},
+  {path: '**', component: NotFoundComponent}
+];
