f70cc8296da9b8bf5b77a0deda3ff9e6735b0dae
Emre Sen <maemresen@yazilim.vip>
2023-11-26 19:37:05 +0200

    FE update (wip)

--- fintrack-fe/src/app/app.component.spec.ts
+++ fintrack-fe/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,11 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should contain swagger link', () => {
+  it(`should have the 'fintrack-fe' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('fintrack-fe');
+  });
+
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    const swaggerLink = compiled.querySelector('#swagger-link');
-    expect(swaggerLink).not.toBeNull();
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fintrack-fe');
   });
 });
