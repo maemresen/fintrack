0d74216a942eb698089977695ffa652694b7be73
Mehmet Emre Sen <maemresen@yazilim.vip>
2024-08-07 14:29:38 +0300

    layout configured

--- app/fintrack-ui/src/app/app.component.ts
+++ app/fintrack-ui/src/app/app.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {Button} from "primeng/button";
+import {Component, OnInit} from '@angular/core';
+import {AvatarModule} from "primeng/avatar";
+import {MenuModule} from "primeng/menu";
+import {MenuItem} from "primeng/api";
+import {BadgeModule} from "primeng/badge";
+import {RippleModule} from "primeng/ripple";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Button],
+  imports: [MenuModule, BadgeModule, RippleModule, AvatarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        separator: true
+      },
+      {
+        label: 'Documents',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-plus',
+            shortcut: '⌘+N'
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-search',
+            shortcut: '⌘+S'
+          }
+        ]
+      },
+      {
+        label: 'Profile',
+        items: [
+          {
+            label: 'Settings',
+            icon: 'pi pi-cog',
+            shortcut: '⌘+O'
+          },
+          {
+            label: 'Messages',
+            icon: 'pi pi-inbox',
+            badge: '2'
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            shortcut: '⌘+Q'
+          }
+        ]
+      },
+      {
+        separator: true
+      }
+    ];
+  }
 }
