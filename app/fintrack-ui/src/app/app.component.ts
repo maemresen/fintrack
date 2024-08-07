e5643a0af1ffbfad3c322b01f494fc48eb42139f
Mehmet Emre Sen <maemresen@yazilim.vip>
2024-08-07 14:37:46 +0300

    side-menu component separated

--- app/fintrack-ui/src/app/app.component.ts
+++ app/fintrack-ui/src/app/app.component.ts
@@ -1,63 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {AvatarModule} from "primeng/avatar";
 import {MenuModule} from "primeng/menu";
-import {MenuItem} from "primeng/api";
 import {BadgeModule} from "primeng/badge";
 import {RippleModule} from "primeng/ripple";
+import {SideMenuContainer} from "./container/side-menu/side-menu.container";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuModule, BadgeModule, RippleModule, AvatarModule],
+  imports: [MenuModule, BadgeModule, RippleModule, AvatarModule, SideMenuContainer],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  items: MenuItem[] | undefined;
-
-  ngOnInit() {
-    this.items = [
-      {
-        separator: true
-      },
-      {
-        label: 'Documents',
-        items: [
-          {
-            label: 'New',
-            icon: 'pi pi-plus',
-            shortcut: '⌘+N'
-          },
-          {
-            label: 'Search',
-            icon: 'pi pi-search',
-            shortcut: '⌘+S'
-          }
-        ]
-      },
-      {
-        label: 'Profile',
-        items: [
-          {
-            label: 'Settings',
-            icon: 'pi pi-cog',
-            shortcut: '⌘+O'
-          },
-          {
-            label: 'Messages',
-            icon: 'pi pi-inbox',
-            badge: '2'
-          },
-          {
-            label: 'Logout',
-            icon: 'pi pi-sign-out',
-            shortcut: '⌘+Q'
-          }
-        ]
-      },
-      {
-        separator: true
-      }
-    ];
-  }
+export class AppComponent {
 }
---
+++ app/fintrack-ui/src/app/container/side-menu/side-menu.container.ts
@@ -0,0 +1,68 @@
+import {Component, OnInit} from '@angular/core';
+import {BadgeModule} from "primeng/badge";
+import {MenuModule} from "primeng/menu";
+import {MenuItem} from "primeng/api";
+import {Ripple} from "primeng/ripple";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-side-menu-container',
+  standalone: true,
+  imports: [
+    BadgeModule,
+    MenuModule,
+    Ripple,
+    NgIf
+  ],
+  templateUrl: './side-menu.container.html',
+})
+export class SideMenuContainer implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        separator: true
+      },
+      {
+        label: 'Documents',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-plus',
+            shortcut: '⌘+N'
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-search',
+            shortcut: '⌘+S'
+          }
+        ]
+      },
+      {
+        label: 'Profile',
+        items: [
+          {
+            label: 'Settings',
+            icon: 'pi pi-cog',
+            shortcut: '⌘+O'
+          },
+          {
+            label: 'Messages',
+            icon: 'pi pi-inbox',
+            badge: '2'
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            shortcut: '⌘+Q'
+          }
+        ]
+      },
+      {
+        separator: true
+      }
+    ];
+  }
+
+}
